d5e1e684aea29520806819de1b859c5c5551da1d
denstor <132818209+denstor@users.noreply.github.com>
2023-05-17 09:34:51 +0200

    Update 0515_hypothesis_testing (final).py

--- 0515_hypothesis_testing (final).py
+++ 0515_hypothesis_testing (final).py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dirname=".\Titanic_cases\database\\"
+dirname=".\cases\database\\"
 df=pd.read_excel(dirname+"cata.xlsx")
 
 # print(df.shape)
