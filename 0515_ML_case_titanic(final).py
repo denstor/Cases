1d7f1dd6ae9a3a77a247bc8ee9c423f36af74d78
denstor <132818209+denstor@users.noreply.github.com>
2023-05-17 09:30:24 +0200

    Add files via upload

---
+++ 0515_ML_case_titanic(final).py
@@ -0,0 +1,141 @@
+# Load libraries
+# import sys
+# import scipy
+# import numpy
+# import matplotlib
+# import pandas
+# import sklearn
+# from pandas import read_csv
+
+# Load libraries
+import pandas as pd
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+
+dirname=".\Titanic_cases\database\\"
+dataset=pd.read_excel(dirname+"cata.xlsx")
+
+dataset=dataset.drop(['name','sibsp', 'parch', 'ticket', 'fare', 
+                      'cabin', 'embarked', 'boat', 'body', 'home.dest'],axis=1)
+dataset=dataset[['survived','pclass', 'sex', 'age']]
+
+# shape, head, descriptions of the dataset and dropna
+print(dataset.shape)
+print(dataset.head())
+print(dataset.describe())
+dataset=dataset.dropna(axis=0)
+
+# change Y data type for the categorical ("yes"=survived, "no"=not survived) 
+# change X data to numerical ("female"=0, "male"=1)
+# set up data types
+dataset.survived.replace(to_replace=[0,1],value=["no","yes"], inplace=True)
+# dataset.loc[dataset['survived']==0,"survived"]="no"
+# dataset.loc[dataset['survived']==1,"survived"]="yes"
+dataset.sex.replace(to_replace=["female","male"],value=[0,1], inplace=True)
+# dataset.loc[dataset['sex']=="female","sex"]=0
+# dataset.loc[dataset['sex']=="male","sex"]=1
+dataset = dataset.astype({'survived':'category','pclass':'int','sex':'int','age':'float'})
+print(dataset.info())
+
+# EDA (exploratory data analysis) - scatter plot matrix
+# scatter_matrix(dataset)
+# pyplot.show()
+
+# split-out validation dataset
+array = dataset.values
+X = array[:,1:]
+y = array[:,0]
+
+# standardize the data (e.g. the age scale is times higher vs the others)
+object=StandardScaler()
+X=object.fit_transform(X)
+
+# set up the train & test split
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# the goal is to check different models for prediction (linear and non-linear ones)
+# and choose the most accurate one. The list of models is given below:
+# 1. Spot Check Algorithms
+# 2. Logistic Regression (LR)
+# 3. Linear Discriminant Analysis (LDA)
+# 4. K-Nearest Neighbors (KNN)
+# 5. Classification and Regression Trees (CART)
+# 6. Gaussian Naive Bayes (NB)
+# 7. Support Vector Machines (SVM)
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+# evaluate each model in turn and show the final results
+# the Support Vector Machines (SVM) show the best results (correlation and std error)
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %.2f (%.2f)' % (name, cv_results.mean(), cv_results.std()))
+
+# Compare different Algorithms
+# the Support Vector Machines (SVM) show the best results 
+# (correlation - 0.79 and std error - 0.03)
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Make predictions on validation dataset by using SVM model
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions: high accuracy score (0.83) = TP+TN
+# high f1-score (precision, recall)
+# confusion matrix: FPR/T1 error (FP) - 17 (8%), FNR/T2 error (FN) - 19 (9%)
+print("______________________________________________")
+print("accuracy_score")
+print('%.2f' % accuracy_score(Y_validation, predictions))
+print("______________________________________________")
+print("confusion_matrix")
+print(confusion_matrix(Y_validation, predictions))
+print("______________________________________________")
+print("classification_report")
+print(classification_report(Y_validation, predictions))
+
+# Evaluate all the X data by using SVM model, high accuracy score (0.81)
+predictions2=model.predict(X)
+print("______________________________________________")
+print("accuracy_score for all X data")
+print(round(sum(y==predictions2)/len(y),2))
+
+print("________________________________________________")
+
+# now let's quantify the impact of factors (pclacc,sex,age) on survival
+# Linear Regression Feature Importance (the data was standardized in the very beginning)
+# the highest impact on survival makes sex (-1.17)
+# followed by pclass (-0.83) and finally age (-0.47)
+
+model_feature = LogisticRegression(solver='liblinear', multi_class='ovr')
+model_feature.fit(X_train, Y_train)
+importance=model_feature.coef_
+cols=list(dataset.columns[1:])
+for i in range(len(cols)):
+    print('%s: %.2f' % (cols[i],importance[0,i]))
---
+++ 0515_hypothesis_testing (final).py
@@ -0,0 +1,96 @@
+#the given below case shows the logics of the test of the hypothesis
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dirname=".\Titanic_cases\database\\"
+df=pd.read_excel(dirname+"cata.xlsx")
+
+# print(df.shape)
+# print(df.columns)
+# print(df.describe())
+# print(df.head())
+
+df=df.drop(['name','sibsp', 'parch', 'ticket', 
+       'fare', 'cabin', 'embarked', 'boat', 'body', 'home.dest'],axis=1)
+
+
+df=df.dropna(axis=0)
+df = df.astype({'sex':'category', 'survived':'int','pclass':'int','age':'float'})
+print(df.describe())
+print(df.info())
+
+print("_____________________________________________________________")
+#the more women and children survived
+age=pd.cut(df["age"],[0,18,30,40,80])
+print(df.pivot_table("survived",["sex",age],"pclass"))
+
+print("_____________________________________________________________")
+#the data shows clear evidence of higher chances of survival for the passengers of cl1
+#although the two groups of survivors: Female&Class1 and Female&Class2 have negligible difference (96% vs 89%)
+#the question I'm going to investigate further is whether this difference is statistically significant
+print(df.pivot_table("survived",index="sex",columns="pclass",margins=True))
+
+print("_____________________________________________________________")
+
+#H1 hypothesis - there is a difference in chances to survive between cl1 & cl2
+#H0 - there is no difference between these two groups
+#to prove H0 I'm going use permutation samples of the empirical dataset
+
+#the function to generate many sets of simulated data assuming H0 is true
+def draw_perm_reps(data1,data2,func,size=1):
+    perm_replicates=np.empty(size)
+    for i in range(size):
+        perm_sample1,perm_sample2=permutation_sample(data1,data2)
+        perm_replicates[i]=func(perm_sample1,perm_sample2)
+    return perm_replicates
+
+#the function to calculcate difference of means
+def diff_of_means(data1,data2):
+    diff=np.mean(data1)-np.mean(data2)
+    return diff
+
+#the function to implement permutation of the sample
+def permutation_sample(data1,data2):
+    data=np.concatenate((data1,data2))
+    permuted_data=np.random.permutation(data)
+    perm_sample1=permuted_data[:len(data1)]
+    perm_sample2=permuted_data[len(data1):]
+    return perm_sample1,perm_sample2
+
+#the code below splits out the two target groups
+cl1=df["survived"][(df["sex"]=="female") & (df["pclass"]==1)]
+# cl1=cl1.to_numpy()
+cl1=cl1.values
+
+
+cl2=df["survived"][(df["sex"]=="female") & (df["pclass"]==2)]
+# cl2=cl2.to_numpy()
+cl2=cl2.values
+
+np.random.seed(123)
+empirical_diff_means=diff_of_means(cl1,cl2)
+print("empirical_diff_means - ",np.round(empirical_diff_means,2))
+perm_replicates=draw_perm_reps(cl1,cl2,diff_of_means,1000)
+# print("perm_replicates: ",perm_replicates)
+
+#p-value is the fraction of the simulated data sets for which 
+#the test statistic is at least as extreme as for the empirical dataset
+p=np.sum(perm_replicates>=empirical_diff_means)/len(perm_replicates)
+print("p-value: ",np.round(p,2))
+
+#p=0.03 - 3% of simulated data has higher than empirical difference of means (7%)
+#the results of the statitics let us decline H0 
+#and therefore prove H1 - there is a difference between cl1 and cl2
+
+#the chart showing distribution of the permutated means with probability
+weights=np.ones_like(perm_replicates/len(perm_replicates))
+plt.hist(perm_replicates,weights=weights)
+plt.xlabel("distribution of differences of the permutated means")
+plt.ylabel("probability")
+plt.show()
+
+#although the two groups of survivors: Female&Class1 and Female&Class2 have negligible difference (96% vs 89%)
+#the experiment proved that the difference is statistically significant
+#which means that Female&Class1 have higher chances to survive vs Female&Class2
